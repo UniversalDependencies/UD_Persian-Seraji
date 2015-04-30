753cecaaa684f1d4c151b01b08322332eeac876e
Filip Ginter <ginter@cs.utu.fi>
2015-04-30 11:36:23 +0300

    Clitic split works, but their prefixing not

--- split.py
+++ split.py
@@ -1,11 +1,22 @@
+# -*- coding:utf-8 -*-
 import sys
 import codecs
 import re
 import os
 
 SCRIPTDIR=os.path.dirname(os.path.abspath(__file__))
-
 out8=codecs.getwriter("utf-8")(sys.stdout)
+
+
+pron_re=re.compile(u'^(م|ت|ش|مان|تان|شان|م|ام|ی|ای|ه|ست|یم|ایم|ید|اید|ند|اند)',re.U)
+
+def cut_clitic(s):
+    match=pron_re.match(s)
+    if match==None:
+        return s,u"???"
+    else:
+        assert len(s[match.end():])+len(s[:match.end()])==len(s)
+        return s[match.end():],s[:match.end()]
 
 def read_conll(inp,maxsent):
     """ Read conll format file and yield one sentence at a time as a list of lists of columns. If inp is a string it will be interpreted as filename, otherwise as open file for reading in unicode"""
@@ -57,11 +68,10 @@ ID,FORM,LEMMA,CPOS,POS,FEAT,HEAD,DEPREL,DEPS,MISC=range(10)
 def split_clitics(sent,comments):
     new_sent=[]
     tokens=[] #list of words that initiate a two-word token
-    offsets=[0 for _ in sent]
-    counter=0
-    for idx,line in enumerate(sent):
+    offsets=[0 for _ in sent] #used to renumber tokens
+    headmap=[0 for _ in sent] #reassignment of head IDs (offset) used to correct the heads
+    for sent_idx,line in enumerate(sent):
         assert len(line)==10
-        counter+=1
         deps=parts(line[DEPREL])
         if len(deps)==1: #nothing to do
             new_sent.append(line)
@@ -69,30 +79,34 @@ def split_clitics(sent,comments):
         else:
             #Need to split
             #comments.append(u"###TOKEN###%d-%d\t%s"%(int(line[0]),int(line[0]+1),line[1])+(u"\t_"*7))
-            new_sent.append(line[:])
-            new_sent[-1][ID]=unicode(counter)
-            counter+=1
-            new_sent.append(line[:])
-            new_sent[-1][ID]=unicode(counter)
-            for idx in range(int(line[ID])+1,len(sent)):
+            tok,clit=cut_clitic(line[FORM])
+            new_sent.append(line[:]) 
+            new_sent[-1][FORM]=clit
+            new_sent[-1][LEMMA]=u"_"
+            new_sent[-1][FEAT]=u"_"
+            new_sent[-1][POS]=u"_"
+            new_sent[-1][CPOS]=u"CHECK"
+            new_sent[-1][DEPREL]=deps[0]
+            for idx in range(sent_idx,len(sent)):
                 offsets[idx]+=1
-            if deps[2]=="LEFT":
-                new_sent[-2][DEPREL]=deps[0]
+            
+            #Now add the original token (sans the clitic: todo)
+            new_sent.append(line[:])
+            #And now decide on the heads
+            if deps[2]==u"LEFT": #The left is the head (ie the newly created token)
+                headmap[sent_idx]=-1
+                new_sent[-1][HEAD]=unicode(sent_idx+1)
                 new_sent[-1][DEPREL]=u"SPLTL:"+deps[1]
-                new_sent[-1][HEAD]=line[ID]
-                new_sent[-1][FORM]=u"???"
-                new_sent[-1][FEAT]=u"_"
             elif deps[2]=="RIGHT":
-                new_sent[-1][DEPREL]=deps[0]
                 new_sent[-2][DEPREL]=u"SPLTR:"+deps[1]
-                new_sent[-2][HEAD]=unicode(int(line[ID])+1)
-                new_sent[-2][FORM]=u"???"
-                new_sent[-2][FEAT]=u"_"
+                new_sent[-2][HEAD]=unicode(sent_idx+1)
 
     #Renumber all heads
     for idx,l in enumerate(new_sent):
         if l[HEAD]!=u"0":
-            l[HEAD]=unicode(int(l[HEAD])+offsets[int(l[HEAD])-1])
+            head_0=int(l[HEAD])-1
+            head_0+=offsets[head_0]+headmap[head_0]
+            l[HEAD]=unicode(head_0+1)
         l[ID]=unicode(idx+1)
     return new_sent
             
